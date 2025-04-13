29d3ff13e87adc1a354c469c5be55fe8ed48d15c
Tusharkamthe23 <139905124+Tusharkamthe23@users.noreply.github.com>
2025-04-13 15:31:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,6 +71,11 @@ if password == CORRECT_PASSWORD:
                     'quiet': True,
                     'outtmpl': temp_path,
                     'format': f"{selected_format['format_id']}",
+                    'http_headers': {
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                                      'AppleWebKit/537.36 (KHTML, like Gecko) '
+                                      'Chrome/122.0.0.0 Safari/537.36',
+                        'Accept-Language': 'en-US,en;q=0.9',
                 }
 
                 with YoutubeDL(ydl_opts) as ydl:
